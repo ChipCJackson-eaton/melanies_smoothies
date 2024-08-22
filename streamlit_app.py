3e26b49fd323254dc29bc2e1d7a8c0fe44b2aac1
Chip Jackson <65039921+ChipCJackson-eaton@users.noreply.github.com>
2024-08-22 13:47:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions importcol
+from snowflake.snowpark.functions import col
 #from snowflake.snowpark.context import get_active_session
 #from snowflake.snowpark.functions import col
 
